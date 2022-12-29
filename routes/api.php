90a09206b696ed0ac8888b5cbaba02ec215612d0
AmineZCS <medamineben19@gmail.com>
2022-12-29 20:26:54 +0100

    login/logout and Access tokens

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Dompdf\Dompdf;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,11 +17,46 @@ use Dompdf\Dompdf;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+// get user info
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/pdf', function (Request $request) {
+
+
+
+
+// login and return token
+
+Route::post('/sanctum/token', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+        'device_name' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->device_name)->plainTextToken;
+});
+
+
+
+// logout and delete token
+Route::middleware('auth:sanctum')->get('/user/revoke', function (Request $request) {
+    $user = $request->user();
+    return $user->tokens()->delete();
+});
+
+
+
+// generate pdf and download it
+Route::middleware('auth:sanctum')->get('/pdf', function (Request $request) {
     $dompdf = new Dompdf();
 $dompdf->loadHtml('hello world');
 
