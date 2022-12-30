fab7cf214d5c52b03de3fe9c2bbfdce3f0613960
AmineZCS <medamineben19@gmail.com>
2022-12-30 19:07:26 +0100

    testing generate pdf function

---
+++ app/Http/Controllers/PDFController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+use Barryvdh\DomPDF\Facade\Pdf;
+
+use App\Models\User;
+use App\Models\Viva;
+use Illuminate\Http\Request;
+
+class PDFController extends Controller
+{
+    public function generatePDF(Request $request){
+        $code = $request->code;
+        // $full_Name = $request->user()->name;
+        $viva = Viva::where('code', $code)->get();
+        $data_array = ['name' => $viva[0]['project_name'] ,
+        'year' => $viva[0]['year'] ,
+        'sup_mark' => $viva[0]['sup_mark'] ,
+         'pre_mark'=> $viva[0]['pre_mark'] ,
+         'exa_mark' => $viva[0]['exa_mark'],
+           'sup_name' => $viva[0]['sup_name'],
+           'pre_name' => $viva[0]['pre_name'],
+           'exa_name' => $viva[0]['exa_name'],
+           'final_mark' => $viva[0]['final_mark'],
+           'students' => $viva[0]['students'],
+           'code' => $code
+        //    ,
+        //    'full_name' => $full_Name
+    ];
+    $pdf = PDF::loadView('pdf', $data_array);
+    
+    return $pdf->stream();
+    }
+}
